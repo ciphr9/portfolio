66e12f6ddebf1fdf33b335438252c07c199f4262
ciphr9 <dtb.brandl@live.de>
2023-03-12 11:46:30 +0100

    minor renamings

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Neon</h1>
     </div>
   );
 }
