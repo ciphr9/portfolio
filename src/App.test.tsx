a59221f8438dadecfebb4bef8ec5ffe3e10ee480
ciphr9 <dtb.brandl@live.de>
2023-03-12 12:41:22 +0100

    setup react testing library with first example test

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("displays Neon in headline", () => {
+  render(<App />);
+  const headline = screen.getByRole("heading");
+  expect(headline).toHaveTextContent("Neon");
+});
