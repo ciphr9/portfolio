b4c7c38437651991242bfca88bb8f83b5709dd15
ciphr9 <dtb.brandl@live.de>
2023-04-02 22:31:33 +0200

    minor style and logo changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import ThemeButton from './components/ThemeButton';
-import danielLogo from './assets/daniel.png';
+import logo from './assets/logo.png';
 import LinkedinButton from './components/LinkedinButton';
 import GithubButton from './components/GithubButton';
 import XingButton from './components/XingButton';
 
 function App() {
   return (
-    <div className="cursor-default flex flex-col h-screen bg-slate-800 bg-gradient-to-tr from-slate-500 via-white to-slate-300 text-black dark:from-black dark:via-black dark:to-slate-900 dark:text-slate-300">
+    <div className="select-none cursor-default flex flex-col h-screen bg-slate-800 bg-gradient-to-tr from-slate-500 via-white to-slate-300 text-black dark:from-black dark:via-black dark:to-slate-900 dark:text-slate-300">
       <div className="flex items-center p-5">
         <p className="flex-1 font-extrabold text-xl bg-clip-text">Portfolio</p>
         <ThemeButton />
       </div>
       <div className="z-10 flex flex-1 flex-col items-center text-center justify-center">
-        <img className="rounded-full h-56 w-56 mb-5 object-cover object-right opacity-100" src={danielLogo} alt="" />
+        <img className="rounded-full h-56 w-56 mb-5" src={logo} alt="logo" />
         <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-cyan-600 via-cyan-400 to-cyan-300">
           Daniel Brandl
         </h1>
