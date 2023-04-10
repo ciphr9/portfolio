52f861ea2a0fdda9345a4968ce030878d1153658
ciphr9 <dtb.brandl@live.de>
2023-04-10 18:51:10 +0200

    vitest mocking webgl so the tests dont fail

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { vi } from 'vitest';
+
+vi.mock('three', async () => {
+  const THREE = await vi.importActual<any>('three');
+  return {
+    ...THREE,
+    WebGLRenderer: vi.fn().mockReturnValue({
+      domElement: document.createElement('div'), // create a fake div
+      setSize: vi.fn(),
+      render: vi.fn(),
+    }),
+  };
+});
 
 test('displays Daniel Brandl in headline', () => {
   render(<App />);
