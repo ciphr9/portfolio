8af7848905b0fdef471f1b4690c1c04f7c724750
ciphr9 <dtb.brandl@live.de>
2023-04-10 18:20:49 +0200

    never empty theme string anymore

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -5,8 +5,10 @@ export enum Theme {
 
 export const setTheme = (theme: Theme) => {
   if (theme === Theme.Dark) {
+    document.documentElement.classList.remove('light');
     document.documentElement.classList.add('dark');
   } else {
     document.documentElement.classList.remove('dark');
+    document.documentElement.classList.add('light');
   }
 };
