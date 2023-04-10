6d1a0ad3c7a96a7a374deb53ce33961492418fb2
ciphr9 <dtb.brandl@live.de>
2023-04-10 18:20:01 +0200

    tests for app added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,24 @@ import App from './App';
 
 test('displays Daniel Brandl in headline', () => {
   render(<App />);
-  const headline = screen.getByRole('heading');
-  expect(headline).toHaveTextContent('Daniel Brandl');
+  const headline = screen.getByText('Daniel Brandl');
+  expect(headline).toBeInTheDocument();
+});
+
+test('displays Software Developer in sub-headline', () => {
+  render(<App />);
+  const subHeadline = screen.getByText('Software Developer');
+  expect(subHeadline).toBeInTheDocument();
+});
+
+test('displays Portfolio in navigation', () => {
+  render(<App />);
+  const navigation = screen.getByText('Portfolio');
+  expect(navigation).toBeInTheDocument();
+});
+
+test('displays logo', () => {
+  render(<App />);
+  const logo = screen.getByRole('img');
+  expect(logo).toBeInTheDocument();
 });
